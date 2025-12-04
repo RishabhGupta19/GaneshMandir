070b8970c831621ac92a38b04e777cecad26658b
RishabhGupta19 <161333480+RishabhGupta19@users.noreply.github.com>
2025-12-04 15:35:57 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,29 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,                // allows access from network + ngrok
+//     strictPort: true,          // keep the port fixed
+//     allowedHosts: [
+//       "jace-hypercyanotic-distantly.ngrok-free.dev" // your ngrok URL host
+//     ]
+//   }
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Sitemap from 'vite-plugin-sitemap'; // Import the sitemap plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({ // Add the Sitemap plugin to the plugins array
+      hostname: 'www.ganeshakhara.com', // Set your website's domain
+    }),
+  ],
   server: {
     host: true,                // allows access from network + ngrok
     strictPort: true,          // keep the port fixed
